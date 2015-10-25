8d1c74d87fc95e4fe128d332af399af2f0cb6627
TE <ryukau@gmail.com>
2015-10-26 03:40:28 +0900

    uhhyou

---
+++ main.cpp
@@ -0,0 +1,32 @@
+﻿#include <QCoreApplication>
+
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    bool b;
+    int i;
+
+    b = 100; // true
+    i = b; // i = 1
+    qDebug() << b << ", " << i;
+
+    b = -100; // true
+    i = b; // i = 1
+    qDebug() << b << ", " << i;
+
+    b = 0; // false
+    i = b; // i = 0
+    qDebug() << b << ", " << i;
+
+    ++b; // 後置または前置++演算子が使える
+    qDebug() << b;
+
+    //--b; // --演算子は使えない
+    // b = 0 / 0; // arithmetic exception
+
+    return a.exec();
+}
+
